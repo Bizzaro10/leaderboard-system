837f0e02f6b5ffe9ed612e52bc1802c72c3209a4
Kira <128256880+Bizzaro10@users.noreply.github.com>
2025-07-22 15:26:50 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, { cors: { origin: "*" } });
 
-const _dirname = path.resolve();
+
 
 app.use(cors());
 app.use(express.json());
